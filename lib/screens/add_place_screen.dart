b020782ec1d28e2643ef673f8474fab378ecb4e8
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2023-09-15 23:16:17 +0530

    Loction Input Setup

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -82,6 +83,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ImageInput(ontakeImage: (image) {
               _takenImage = image;
             }),
+            const SizedBox(height: 16),
+            LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
